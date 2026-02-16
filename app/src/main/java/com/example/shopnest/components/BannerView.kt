02a173ed8b0b75f6745c5e67b926145d6c482a2e
emran <ahmed777emran@gmail.com>
2026-02-16 20:00:58 +0600

    HomeScreen updated

--- app/src/main/java/com/example/shopnest/components/BannerView.kt
+++ app/src/main/java/com/example/shopnest/components/BannerView.kt
@@ -1,32 +1,15 @@
 package com.example.shopnest.components
 
 import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -36,16 +19,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import com.example.shopnest.R
-import com.example.shopnest.screen.NavItems
 import com.google.firebase.Firebase
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.firestore
 import com.tbuonomo.viewpagerdotsindicator.compose.DotsIndicator
 import com.tbuonomo.viewpagerdotsindicator.compose.model.DotGraphic
@@ -57,7 +34,7 @@ import com.tbuonomo.viewpagerdotsindicator.compose.type.ShiftIndicatorType
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HeaderView(modifier: Modifier = Modifier) {
+fun BannerView(modifier: Modifier = Modifier) {
     var bannerList by remember {
         mutableStateOf<List<String>>(emptyList())
     }
@@ -78,30 +55,27 @@ fun HeaderView(modifier: Modifier = Modifier) {
             initialPage = 0,
             pageCount = { bannerList.size }
         )
-
         HorizontalPager(
             state = pagerState,
-            pageSpacing = 24.dp,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
+            modifier = Modifier.padding(2.dp), verticalAlignment = Alignment.CenterVertically
+
         ) { page ->
             AsyncImage(
                 model = bannerList[page],
                 contentDescription = null,
-                modifier = Modifier.fillMaxSize()
-                    .clip(RoundedCornerShape(16.dp))
+                modifier = Modifier.clip(RoundedCornerShape(12.dp))
             )
         }
-        Spacer(modifier.height(5.dp))
+
+        Spacer(modifier = Modifier.height(5.dp))
 
         DotsIndicator(
             dotCount = bannerList.size,
             pagerState = pagerState,
             type = ShiftIndicatorType(
                 DotGraphic(
-                    color = MaterialTheme.colorScheme.primary,
-                    size = 6.dp
+                    color = MaterialTheme.colorScheme.onSurface,
+                    size = 8.dp
                 )
             )
         )
@@ -111,5 +85,5 @@ fun HeaderView(modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun HeaderViewPreview() {
-    HeaderView()
+    BannerView()
 }
---
+++ app/src/main/java/com/example/shopnest/pages/FavouritePage.kt
@@ -0,0 +1,51 @@
+package com.example.shopnest.pages
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+
+/**
+ * @author EMRAN AHMED
+ */
+
+@Composable
+fun FavouritePage(navController: NavHostController, modifier: Modifier = Modifier){
+    Column (
+        modifier = modifier.fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Favourite Screen",
+            style = TextStyle(
+                fontSize = 30.sp,
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.Bold
+            )
+        )
+
+        Spacer(Modifier.height(20.dp))
+
+        Icon(imageVector = Icons.Default.Favorite,
+            contentDescription = null,
+            Modifier.size(200.dp))
+    }
+}
