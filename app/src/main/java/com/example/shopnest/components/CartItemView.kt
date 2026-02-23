f1b7788b11cee79459cdbbf10a97a1a54ff7dc1c
emran <ahmed777emran@gmail.com>
2026-02-23 16:19:38 +0600

    feat(cart): add increase, decrease, remove from cart functionality

--- app/src/main/java/com/example/shopnest/components/CartItemView.kt
+++ app/src/main/java/com/example/shopnest/components/CartItemView.kt
@@ -1,6 +1,30 @@
 package com.example.shopnest.components
 
+import android.util.Log
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -8,11 +32,22 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
 import com.example.shopnest.model.ProductModel
+import com.example.shopnest.utils.AppUtils
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
 
@@ -29,14 +64,10 @@ fun CartItemView(
 ) {
 
     var productModel by remember {
-        mutableStateOf<ProductModel?>(null)
+        mutableStateOf(ProductModel())
     }
 
-    Text(
-        text = "Product ID: $productId, \nQuantity: $quantity",
-        fontWeight = FontWeight.Bold,
-        modifier = Modifier.padding(16.dp)
-    )
+    val context = LocalContext.current
 
     LaunchedEffect(Unit) {
         Firebase.firestore.collection("data")
@@ -44,6 +75,7 @@ fun CartItemView(
             .collection("products")
             .document(productId)
             .get()
+
             .addOnCompleteListener {
                 if(it.isSuccessful){
                     val product = it.result.toObject(ProductModel::class.java)
@@ -55,9 +87,110 @@ fun CartItemView(
             }
     }
 
-    Text(
-        text = "Product Name: ${productModel?.title}, \nPrice: ${productModel?.price}",
-        fontWeight = FontWeight.Normal,
-        modifier = Modifier.padding(16.dp)
-    )
+    Card(
+        modifier = modifier.padding(8.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(8.dp),
+        shape = RoundedCornerShape(12.dp)
+    ) {
+
+        Row (
+            modifier = Modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            AsyncImage(
+                model = productModel.images.firstOrNull(),
+                contentDescription = productModel.title,
+                Modifier
+                    .width(120.dp)
+                    .height(120.dp)
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(12.dp))
+                    .padding(8.dp)
+            )
+
+            Column(
+                Modifier.weight(1f).padding(8.dp)
+            ) {
+
+                Text(
+                    text = productModel.title,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+
+                Row (verticalAlignment = Alignment.CenterVertically){
+                    Text(
+                        text = "$"+ productModel.actualPrice,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                    Spacer(Modifier.width(8.dp))
+                    Text(
+                        text = "$"+ productModel.price,
+                        fontSize = 12.sp,
+                        style = TextStyle(textDecoration = TextDecoration.LineThrough)
+                    )
+                }
+
+                Spacer(Modifier.height(8.dp))
+
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    IconButton(
+                        onClick = {
+                            AppUtils.removeFromCart(context, productModel.id)
+                        }
+                    ) {
+                        Text("-")
+                    }
+
+                    BadgedBox(
+                        badge = {
+                            if(quantity > 0){
+                                Badge(
+                                    containerColor = Color.Red,
+                                    contentColor = Color.White
+                                ){
+                                    Text(quantity.toString())
+                                }
+                            }
+                        }
+                    ) {
+
+                        Icon(
+                            imageVector = Icons.Filled.ShoppingCart,
+                            contentDescription = "Cart",
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+
+                    IconButton(
+                        onClick = {
+                            AppUtils.addToCart(context, productModel.id)
+                        }
+                    ) {
+                        Text("+")
+                    }
+                }
+            }
+
+            IconButton(
+                onClick = {
+                    AppUtils.removeFromCart(context, productModel.id, removeAll = true)
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Remove from Cart",
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
+
+        }
+    }
 }
--- app/src/main/java/com/example/shopnest/utils/AppUtils.kt
+++ app/src/main/java/com/example/shopnest/utils/AppUtils.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.widget.Toast
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.firestore
 
 
@@ -26,12 +27,41 @@ object AppUtils{
             val currentQuantity = currentCart[productId] ?: 0
             val updatedQuantity = currentQuantity + 1
 
-            userDocs.update("cartItems.$productId", updatedQuantity)
+            val updatedCart = mapOf("cartItems.$productId" to updatedQuantity)
+
+            userDocs.update(updatedCart)
                 .addOnCompleteListener { cart->
                     if(cart.isSuccessful){
                         showText(context, "Product added to cart")
                     } else{
                         showText(context, "Failed to add product to cart")
+                    }
+                }
+        }
+    }
+
+    fun removeFromCart(context: Context, productId: String, removeAll: Boolean = false){
+
+        val userDocs = Firebase.firestore.collection("users")
+            .document(FirebaseAuth.getInstance().currentUser?.uid!!)
+
+        userDocs.get().addOnCompleteListener { userInfo->
+            val currentCart = userInfo.result.get("cartItems") as? Map<String, Long> ?: emptyMap()
+            val currentQuantity = currentCart[productId] ?: 0
+            val updatedQuantity = currentQuantity - 1
+
+            val updatedCart =
+                if (updatedQuantity <= 0 || removeAll)
+                    mapOf("cartItems.$productId" to FieldValue.delete())
+            else
+                mapOf("cartItems.$productId" to updatedQuantity)
+
+            userDocs.update(updatedCart)
+                .addOnCompleteListener { cart->
+                    if(cart.isSuccessful){
+                        showText(context, "Product removed from cart")
+                    } else{
+                        showText(context, "Failed to remove product from cart")
                     }
                 }
         }
