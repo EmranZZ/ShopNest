372d5246b60a9c7238dee971da19f4fb92bb4472
emran <ahmed777emran@gmail.com>
2026-02-23 16:18:05 +0600

    feat(cart): add snapshot listener for showing realtime changes

--- app/src/main/java/com/example/shopnest/pages/CartPage.kt
+++ app/src/main/java/com/example/shopnest/pages/CartPage.kt
@@ -1,31 +1,12 @@
 package com.example.shopnest.pages
 
-import android.R.attr.fontWeight
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ShoppingCart
-import androidx.compose.material3.Badge
-import androidx.compose.material3.BadgedBox
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.DividerDefaults
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -33,17 +14,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import coil.compose.AsyncImage
 import com.example.shopnest.components.CartItemView
-import com.example.shopnest.model.ProductModel
 import com.example.shopnest.model.UserModel
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
@@ -54,167 +27,56 @@ import com.google.firebase.firestore.firestore
  */
 
 @Composable
-fun CartPage(navController: NavHostController, modifier: Modifier = Modifier){
+fun CartPage(navController: NavHostController, modifier: Modifier = Modifier) {
 
-    var cartItems by remember {
-        mutableStateOf<Map<String, Long>>(emptyMap())
-    }
-
-    var products by remember {
-        mutableStateOf<List<ProductModel>>(emptyList())
-    }
+//    var cartItems by remember {
+//        mutableStateOf<Map<String, Long>>(emptyMap())
+//    }
 
     var userModel by remember {
         mutableStateOf(UserModel())
     }
 
-    var productName by remember {
-        mutableStateOf<String>("")
-    }
 
-    Column (
+    Column(
         modifier = modifier.fillMaxSize()
             .padding(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        LaunchedEffect(Unit) {
-            Firebase.firestore.collection("users")
+        DisposableEffect(Unit) {
+            val listener = Firebase.firestore.collection("users")
                 .document(FirebaseAuth.getInstance().currentUser?.uid!!)
-                .get()
-                .addOnCompleteListener { it ->
-                    if(it.isSuccessful){
 
-                        val result = it.result.toObject(UserModel::class.java)
+                .addSnapshotListener { it, _ ->
+                    if (it != null && it.exists()){
+                        val result = it.toObject(UserModel::class.java)
 
-                        if (result != null){
+                        if (result != null) {
                             userModel = result
                         }
-
-//                        val cartData = userInfo.result.get("cartItems") as? Map<String, Long> ?: emptyMap()
-//                        if(cartData.isNotEmpty()){
-//                            cartItems = cartData
-//                        }
                     }
                 }
 
-        }
-
-        userModel.cartItems.forEach { (productId, qty) ->
-
-            CartItemView(
-                productId = productId,
-                quantity = qty,
-                modifier = Modifier.padding(8.dp),
-                navController = navController
-            )
-        }
-
-/*
-        cartItems.forEach {(productId, quantity) ->
-
-            Firebase.firestore.collection("data")
-                .document("stock")
-                .collection("products")
-                .get()
-                .addOnCompleteListener { productInfo ->
-                    if(productInfo.isSuccessful){
-
-                        val result = productInfo.result.documents.mapNotNull {
-                            it.toObject(ProductModel::class.java)
-                        }
-                        product = result
-
-                        //val title = productInfo.result.get("title") as? String ?: "Unknown Product"
-                        //productName = title
-                    }
-                }
-*/
-            Card (
-                modifier = Modifier.fillMaxWidth()
-                    .align(Alignment.CenterHorizontally)
-                    .clickable{},
-                elevation = CardDefaults.cardElevation(
-                    defaultElevation = 4.dp,
-                    pressedElevation = 6.dp
-                ),
-                shape = RoundedCornerShape(12.dp)
-            ) {
-
-//                Row(
-//                    Modifier.fillMaxWidth()
-//                ) {
-//                    AsyncImage(
-//                        model = product.images.firstOrNull(),
-//                        contentDescription = product.description,
-//                        Modifier.height(120.dp).padding(12.dp)
-//                    )
-//
-//                    Column(
-//                        verticalArrangement = Arrangement.Center,
-//                        modifier = Modifier.padding(12.dp)
-//                    ) {
-//                        Text(
-//                            text = product.title,
-//                            modifier = Modifier.fillMaxWidth(),
-//                            fontWeight = FontWeight.Bold,
-//                            maxLines = 1,
-//                            overflow = TextOverflow.Ellipsis
-//                        )
-//
-//                        Text(
-//                            text = product.description,
-//                            fontSize = 12.sp,
-//                            modifier = Modifier.fillMaxWidth(),
-//                            maxLines = 1,
-//                            overflow = TextOverflow.Ellipsis
-//                        )
-//
-//                        Spacer(Modifier.height(8.dp))
-//                        HorizontalDivider(thickness = 2.dp)
-//                        Spacer(Modifier.height(8.dp))
-//
-//                        Row(
-//                            modifier = Modifier.fillMaxWidth(),
-//                            verticalAlignment = Alignment.CenterVertically,
-//                            horizontalArrangement = Arrangement.SpaceAround
-//                        ) {
-//                            Text(
-//                                text = "Qty:",
-//                                fontSize = 16.sp,
-//                                fontWeight = FontWeight.SemiBold
-//                            )
-//
-//
-//                            BadgedBox(
-//                                badge = {
-//                                    if (quantity > 0) {
-//                                        Badge(
-//                                            containerColor = Color.Red,
-//                                            contentColor = Color.White
-//                                        ) {
-//                                            Text(text = "$quantity")
-//                                        }
-//                                    }
-//                                }
-//                            ) {
-//                                Icon(
-//                                    imageVector = Icons.Filled.ShoppingCart,
-//                                    contentDescription = "Shopping Cart"
-//                                )
-//                            }
-//
-//                            Button(onClick = { }) {
-//                                Text("Add")
-//                            }
-//                        }
-//                    }
-//
-//                }
-
+            onDispose {
+                listener.remove()
             }
+        }
 
-            Spacer(Modifier.height(20.dp))
-        //}
+        LazyColumn {
+            items(
+                userModel.cartItems.toList(),
+                key = {
+                    it.first
+                }
+            ) { (productId, qty) ->
+                CartItemView(
+                    productId = productId,
+                    quantity = qty,
+                    modifier = Modifier.padding(8.dp),
+                    navController = navController
+                )
+            }
+        }
     }
 }
