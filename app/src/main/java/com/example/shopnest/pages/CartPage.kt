de18c53fd52310f29848bf31118b549e6d8b919e
emran <ahmed777emran@gmail.com>
2026-02-25 16:01:17 +0600

    style(cart): handle empty cart item

--- app/src/main/java/com/example/shopnest/pages/CartPage.kt
+++ app/src/main/java/com/example/shopnest/pages/CartPage.kt
@@ -1,30 +1,20 @@
 package com.example.shopnest.pages
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -33,7 +23,7 @@ import androidx.navigation.NavHostController
 import com.example.shopnest.components.CartItemView
 import com.example.shopnest.model.UserModel
 import com.example.shopnest.navigation.Screen
-import com.example.shopnest.utils.AppUtils
+import com.example.shopnest.utils.Utils
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.firestore
@@ -84,35 +74,51 @@ fun CartPage(navController: NavHostController, modifier: Modifier = Modifier) {
             fontWeight = FontWeight.Bold
         )
 
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth()
-                .weight(1f)
-        ) {
-            items(
-                userModel.cartItems.toList(),
-                key = {
-                    it.first
+        if (userModel.cartItems.isNotEmpty()){
+
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth()
+                    .weight(1f)
+            ) {
+                items(
+                    userModel.cartItems.toList(),
+                    key = {
+                        it.first
+                    }
+                ) { (productId, qty) ->
+                    CartItemView(
+                        productId = productId,
+                        quantity = qty,
+                        navController = navController
+                    )
                 }
-            ) { (productId, qty) ->
-                CartItemView(
-                    productId = productId,
-                    quantity = qty,
-                    navController = navController
+            }
+
+            Button(
+                onClick = {
+                    navController.navigate(Screen.Checkout.route)
+                },
+                Modifier.fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text(
+                    text = "Checkout",
+                    fontSize = 20.sp
                 )
             }
         }
+        else{
+            Column(
 
-        Button(
-            onClick = {
-                navController.navigate(Screen.Checkout.route)
-            },
-            Modifier.fillMaxWidth()
-                .height(50.dp)
-        ) {
-            Text(
-                text = "Checkout",
-                fontSize = 20.sp
-            )
+            ) {
+                Utils.Spacer(16)
+
+                Text(
+                    modifier = Modifier.fillMaxSize(),
+                    text = "No items here",
+                    fontSize = 32.sp
+                )
+            }
         }
     }
 }
