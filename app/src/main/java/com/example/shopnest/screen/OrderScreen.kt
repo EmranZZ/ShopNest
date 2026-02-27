192199e241bc206e1bd8979846354b7f9286c774
MOHAMMAD EMRAN AHMED EMON <ahmed777emran@gmail.com>
2026-02-27 23:57:33 +0600

    refactor(order): scaffold added to order screen

--- app/src/main/java/com/example/shopnest/screen/OrderScreen.kt
+++ app/src/main/java/com/example/shopnest/screen/OrderScreen.kt
@@ -7,7 +7,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -18,6 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -32,6 +38,7 @@ import com.google.firebase.firestore.firestore
  * @author EMRAN AHMED
  */
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OrderScreen(navController: NavHostController, modifier: Modifier){
 
@@ -50,42 +57,64 @@ fun OrderScreen(navController: NavHostController, modifier: Modifier){
             }
     }
 
-    Column(
-        modifier = modifier.fillMaxSize()
-            .padding(16.dp)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        "ShopNest",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(end = 16.dp),
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
+                    titleContentColor = MaterialTheme.colorScheme.onSurface
+                )
+            )
+        }
     ) {
-        Text(
-            text = "Your Order",
-            fontSize = 22.sp,
-            fontWeight = FontWeight.Bold
-        )
 
-        if (orderList.isNotEmpty()){
+        Column(
+            modifier = modifier.fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "Your Order",
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold
+            )
 
-            LazyColumn(
-                modifier = Modifier.fillMaxWidth()
-                    .weight(1f)
-            ) {
-                items(
-                    orderList.toList()
-                ){
-                    OrderView(it, navController, modifier)
+            if (orderList.isNotEmpty()){
+
+                LazyColumn(
+                    modifier = Modifier.fillMaxWidth()
+                        .weight(1f)
+                ) {
+                    items(
+                        orderList.toList()
+                    ){
+                        OrderView(it, navController, modifier)
+                    }
                 }
             }
-        }
 
-        else{
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-
-                Text(
+            else{
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    text = "No orders here",
-                    fontSize = 28.sp
-                )
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                    Text(
+                        modifier = Modifier.fillMaxSize(),
+                        text = "No orders here",
+                        fontSize = 28.sp
+                    )
+                }
             }
         }
     }
